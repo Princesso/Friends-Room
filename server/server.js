881297b52de747d406fda66c0c3b5d9775de5239
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-02-20 05:52:51 +0100

    Add connect and disconnect event emitters using socket.io.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const path = require('path');
 const express = require('express');
-
+const socketIO = require('socket.io')
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 8086;
 const app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('new Client connected');
+
+  
+  socket.on('disconnect', () => {
+    console.log('Disconnected from cllient')
+  })
+})
+
 
 app.use(express.static(publicPath))
 // app.get('/', function(req,res){
@@ -13,6 +26,6 @@ app.use(express.static(publicPath))
 //     message: 'Friend\'s room',
 //   })
 // })
-app.listen(port, function(){
+server.listen(port, function(){
   console.log(`app listening at ${port}`)
 })
